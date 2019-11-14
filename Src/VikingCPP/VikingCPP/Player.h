778d4ee509c21840b42b171ab751320c36870296
MarcOlivierLajeunesse <malajeun@gmail.com>
2019-11-14 15:10:47 -0700

    set-up visual studio project

---
+++ Src/VikingCPP/VikingCPP/Player.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <core/Godot.hpp>
+#include <KinematicBody2D.hpp>
+#include <Input.hpp>
+
+
+namespace godot {
+	class Player : public KinematicBody2D {
+		GODOT_CLASS(Player, KinematicBody2D)
+	private:
+	public:
+	};
+}
