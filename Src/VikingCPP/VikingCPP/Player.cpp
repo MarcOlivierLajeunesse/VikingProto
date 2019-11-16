e39e808b210b33ca049b3280988009dce63e4c88
MarcOlivierLajeunesse <malajeun@gmail.com>
2019-11-15 19:26:38 -0700

    player rotates with mouse

--- Src/VikingCPP/VikingCPP/Player.cpp
+++ Src/VikingCPP/VikingCPP/Player.cpp
@@ -26,16 +26,21 @@ void Player::_process(float delta) {
 void Player::updateMotionFromInput() {
 	motion = Vector2(0, 0);
 	Input* in = Input::get_singleton();
-	if (in->is_action_pressed("move_up")) {
-		motion.y -= SPEED;
+	look_at(get_global_mouse_position());
+	if (in->is_action_pressed("move_forward")) {
+		//motion.y -= SPEED;
+		motion = Vector2(SPEED, 0).rotated(get_rotation());
 	}
-	if (in->is_action_pressed("move_down")) {
-		motion.y += SPEED;
+	if (in->is_action_pressed("move_back")) {
+		//motion.y += SPEED;
+		motion = Vector2(-SPEED, 0).rotated(get_rotation());
 	}
 	if (in->is_action_pressed("move_left")) {
-		motion.x -= SPEED;
+		//motion.x -= SPEED;
+		motion = Vector2(0, -SPEED).rotated(get_rotation());
 	}
 	if (in->is_action_pressed("move_right")) {
-		motion.x += SPEED;
+		//motion.x += SPEED;
+		motion = Vector2(0, SPEED).rotated(get_rotation());
 	}
 }
