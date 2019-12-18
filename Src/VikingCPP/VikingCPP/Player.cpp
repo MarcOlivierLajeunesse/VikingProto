11cf34a12a98be1605a3e9c7360310b9db556089
MarcOlivierLajeunesse <malajeun@gmail.com>
2019-12-18 11:11:25 -0700

    added loot scene, and player has 2 possible movement types

--- Src/VikingCPP/VikingCPP/Player.cpp
+++ Src/VikingCPP/VikingCPP/Player.cpp
@@ -23,24 +23,59 @@ void Player::_process(float delta) {
 	move_and_slide(motion);
 }
 
+
+// 2 versions of input. 
+// 1. pure top-down, where the player can look and change directions using the mouse
+// 2. top down with angle (kinda isometric), where the player only moves and attacks using the keyboard. The PC can only face in 4 discrete directions
+
+//void Player::updateMotionFromInput() {
+//	// version 1
+//	motion = Vector2(0, 0);
+//	Input* in = Input::get_singleton();
+//	look_at(get_global_mouse_position());
+//	if (in->is_action_pressed("move_forward")) {
+//		//motion.y -= SPEED;
+//		motion = Vector2(SPEED, 0).rotated(get_rotation());
+//	}
+//	if (in->is_action_pressed("move_back")) {
+//		//motion.y += SPEED;
+//		motion = Vector2(-SPEED, 0).rotated(get_rotation());
+//	}
+//	if (in->is_action_pressed("move_left")) {
+//		//motion.x -= SPEED;
+//		motion = Vector2(0, -SPEED).rotated(get_rotation());
+//	}
+//	if (in->is_action_pressed("move_right")) {
+//		//motion.x += SPEED;
+//		motion = Vector2(0, SPEED).rotated(get_rotation());
+//	}
+//}
+
 void Player::updateMotionFromInput() {
+	// version 2
+
+	//TODO use states and sprite sheets to determine direction player is looking in
 	motion = Vector2(0, 0);
 	Input* in = Input::get_singleton();
-	look_at(get_global_mouse_position());
+
 	if (in->is_action_pressed("move_forward")) {
 		//motion.y -= SPEED;
-		motion = Vector2(SPEED, 0).rotated(get_rotation());
+		//look_at(Vector2(0, -1));
+		motion = Vector2(0, -SPEED);
 	}
 	if (in->is_action_pressed("move_back")) {
 		//motion.y += SPEED;
-		motion = Vector2(-SPEED, 0).rotated(get_rotation());
+		//look_at(Vector2(0, 1));
+		motion = Vector2(0, SPEED);
 	}
 	if (in->is_action_pressed("move_left")) {
 		//motion.x -= SPEED;
-		motion = Vector2(0, -SPEED).rotated(get_rotation());
+		//look_at(Vector2(-1, 0));
+		motion = Vector2(-SPEED, 0);
 	}
 	if (in->is_action_pressed("move_right")) {
 		//motion.x += SPEED;
-		motion = Vector2(0, SPEED).rotated(get_rotation());
+		//look_at(Vector2(1, 0));
+		motion = Vector2(SPEED, 0);
 	}
 }
